9689dfc152f6fd8a1d6b7ca0d7483fdcc97a15d2
dhiflaoui <belgacem.dhiflaoui@gmail.com>
2024-10-26 16:53:08 +0100

    Feat: config project & install dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        weather_primary: "#00668A",
+        weather_secondary: "#004E71",
+      },
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
+  },
+  plugins: [],
+};
